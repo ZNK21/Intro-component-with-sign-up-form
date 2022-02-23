7835b09988bc8321fd43bf6529ca046f70e4c9b2
znk21 <vincentbruna.oficio@gmail.com>
2022-02-23 02:09:54 -0300

    Revert "End"
    This reverts commit ee05f16fbd7b86d9ac09b60891ceaf24c2774655.

--- JS/script.js
+++ JS/script.js
@@ -1,55 +1,39 @@
 $(function () {
 });
 
-var x = "ok"
-function validacion() {
+function borderojo() {
 
     var email = $(".email").val();
 
-    if ($(".nombre").val() === "" || null) {
-        $("input").css("border", "2px solid red");
-        x = "not";
-    } 
-    else if ($(".apellido").val() === "" || null) {
-        $("input").css("border", "2px solid red");
-        x = "not";
+    if ($(".nombre").val() == "" || null) {
+        $(".nombre").css("border", "2px solid red");
+    } else {
+        $(".nombre").css("border", "2px solid darkgray");
     }
-    else if (email === "" || null || !validateEmail($(".email").val())) {
-        $("input").css("border", "2px solid red");
-        $(".email").attr("placeholder", "Email@example.com");
-        x = "not";
-    }
-    else if ($(".pass").val() === "" || null) {
-        $("input").css("border", "2px solid red");
-        x = "not";
-    }
-    else {
-        $("input").css("border", "2px solid darkgray"); 
-        x = "ok"
-    }//PROBLEMA CON EL ORDEN DE PASS Y EMAIL
 
 
+    if ($(".apellido").val() == "" || null) {
+        $(".apellido").css("border", "2px solid red");
+    } else {
+        $(".apellido").css("border", "2px solid darkgray");
+    }
 
-    
+
+    if ($(".pass").val() == "" || null) {
+        $(".pass").css("border", "2px solid red");
+    } else {
+        $(".pass").css("border", "2px solid darkgray");
+    }
+
+    if (email == "" || null || !validateEmail($(".email").val())){
+        $(".email").css("border", "2px solid red");
+        $(".email").attr("placeholder", "email@ejemplo.com");
+    }else{
+        $(".email").css("border", "2px solid darkgray");
+    }
 }
-
-$(".claim").click(function () {
-    validacion();
-    if(x === "ok"){
-        $(".formulario").submit();
-        sessionStorage.setItem("user", ($(".nombre").val()))
-        sessionStorage.setItem("mail", ($(".email").val()))
-        $(".formulario").trigger("reset");
-    }
-    else{
-        alert("Revisa los datos ingresados")
-    }
-
-
-
-});
 
 function validateEmail(email) {
     var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(email);
-}
+    }
--- JS/script2.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-    var user = sessionStorage.getItem("user");
-    var mail = sessionStorage.getItem("mail");
-    $("#result").text("Hola " + user + " se enviara una confirmacion a tu correo: " + mail + " " ); 
-});
